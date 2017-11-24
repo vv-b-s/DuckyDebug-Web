1e9c1cb38ecf8fcf278ca05e0c4e34b2ca6b27d1
vv-b-s <valentin.v1997@gmail.com>
2017-11-24 13:51:46 +0200

    Fixed problem with building jars. To build jar use 'mvn-package'

--- src/main/java/duckydebug/project/DuckyDebugApplication.java
+++ src/main/java/duckydebug/project/DuckyDebugApplication.java
@@ -1,9 +1,17 @@
 package duckydebug.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
+@EnableScheduling
 public class DuckyDebugApplication {
 
 	public static void main(String[] args) {
